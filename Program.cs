baa63cdacb72aff228d4fbb3f7b587290b85356d
shohincoding-com <salomov.shokhin@gmail.com>
2020-04-13 14:26:15 +0500

    added new class 'Rectangle' in task_2

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace HW_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("side1 = ");
+            double side1 = double.Parse(Console.ReadLine());
+            Console.Write("side2 = ");
+            double side2 = double.Parse(Console.ReadLine());
+        }
+    }
+
+    class Rectangle
+    {
+
+    }
+
+
+    
+}
